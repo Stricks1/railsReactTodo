eeac1adf822beda3d5d41094a064e108b7cdc34f
Stricks1 <gmalheiross@gmail.com>
2020-10-26 17:40:23 -0300

    feat(server): added gem rack-cors to access data on client side

--- server/config/application.rb
+++ server/config/application.rb
@@ -19,5 +19,15 @@ module Server
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: %i[get patch put delete post options],
+        )
+      end
+    end
   end
 end
